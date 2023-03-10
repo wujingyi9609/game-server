c912ec3689c37667bf4f429ba830ef7209b5912b
wujingyi9609 <wujingyi9609@qq.com>
2023-03-10 17:12:48 +0800

    去掉没用的Component

--- src/main/java/com/yi/rpc/handler/messagehandler/ReqMessageRegister.java
+++ src/main/java/com/yi/rpc/handler/messagehandler/ReqMessageRegister.java
@@ -6,12 +6,10 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
 import java.util.List;
 
-@Component
 public class ReqMessageRegister implements BeanPostProcessor, ApplicationContextAware {
     private ApplicationContext applicationContext;
 
