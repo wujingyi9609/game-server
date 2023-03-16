df6c6a5adaa9f62e09fbfca14c32b6bdec6a100e
wujingyi9609 <wujingyi9609@qq.com>
2023-03-16 19:27:15 +0800

    Session

---
+++ src/main/java/com/yi/rpc/session/Session.java
@@ -0,0 +1,30 @@
+package com.yi.rpc.session;
+
+import io.netty.channel.Channel;
+import lombok.Getter;
+
+import java.net.SocketAddress;
+
+@Getter
+public class Session {
+    /**
+     * 客户端channel
+     */
+    private Channel channel;
+    /**
+     * 客户端ip
+     */
+    private String ip;
+    /**
+     * 客户端端口
+     */
+    private int port;
+
+    public Session(Channel channel) {
+        this.channel = channel;
+        SocketAddress clientAddress = channel.remoteAddress();
+        String[] ipAndPort = clientAddress.toString().split(":");
+        this.ip = ipAndPort[0];
+        this.port = Integer.parseInt(ipAndPort[1]);
+    }
+}
